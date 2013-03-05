3efbaec4412ca7f12b9d6e3e8eee2d15f4c4ea57
David Vezzani <david_vezzani@yahoo.com>
2013-03-04 22:40:01 -0800

    sets the hostname value for production

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,4 +1,10 @@
 <?php
+#$dev_server = preg_replace('/:.*/',"", $_SERVER['HTTP_HOST']);
+$dev_server = "my401k.vezzaniphotography.com"
+
+define('WP_SITEURL', "http://$dev_server");
+define('WP_HOME', "http://$dev_server");
+
 $WP_ENVIRONMENT = array(
 	'db_name' => '',
 	'db_user' => '',
