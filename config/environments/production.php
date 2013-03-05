1cf0c114f8bf573008f1cf4a9fb08acebea51620
David Vezzani <david_vezzani@yahoo.com>
2013-03-05 00:22:12 -0700

    synchs with production

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,14 +1,8 @@
 <?php
-#$dev_server = preg_replace('/:.*/',"", $_SERVER['HTTP_HOST']);
-$dev_server = "my401k.vezzaniphotography.com"
-
-define('WP_SITEURL', "http://$dev_server");
-define('WP_HOME', "http://$dev_server");
-
 $WP_ENVIRONMENT = array(
-	'db_name' => '',
-	'db_user' => '',
-	'db_password' => '',
+	'db_name' => 'vezzanip_wp4kb',
+	'db_user' => 'vezzanip_wp4kb',
+	'db_password' => 'pass13!#',
 	'db_host' => '',
 	'wp_lang' => '',
 	'wp_debug' => false,
